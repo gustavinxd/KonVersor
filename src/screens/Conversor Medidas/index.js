110a02105853ce7c581742455e5cbab16268b09f
Gustavo Benjamin <gustasbenjamin@gmail.com>
2023-08-30 01:32:21 -0300

    Navigation criada e configurada

--- src/screens/Conversor Medidas/index.js
+++ src/screens/Conversor Medidas/index.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
 export default function ConversorMedidas(){
     return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <View style={[{ flex: 1, justifyContent: 'center', alignItems: 'center' }, styles.container]}>
+        <Text style={styles.p}>Medidas</Text>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: '#0D0D0D',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  p:{
+    color:'#fff'
+  }
+})
--- src/screens/Conversor Moedas/index.js
+++ src/screens/Conversor Moedas/index.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
 export default function ConversorMoedas(){
     return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <View style={styles.container}>
+        <Text style={styles.p}>Moedas</Text>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: '#0D0D0D',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  p:{
+    color:'#fff'
+  }
+})
