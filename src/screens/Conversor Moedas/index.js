7e520bc4597eb66fb25b0b7787796a1201088d79
Gustavo Benjamin <gustasbenjamin@gmail.com>
2023-09-05 20:17:31 -0300

    Update index.js

--- src/screens/Conversor Moedas/index.js
+++ src/screens/Conversor Moedas/index.js
@@ -7,11 +7,12 @@ import {
   Image,
   TextInput
 } from 'react-native';
-import { Octicons } from '@expo/vector-icons';
+import { Octicons, Feather } from '@expo/vector-icons';
 import AlterarValor from '../../components/AlterarValor/index';
 import { fetchConvertor } from '../../services/api/conversor-moedas';
 import DATA_CURRENCY from '../../data';
 import { TextLight, TextRegular } from '../../Fonts/index';
+import Btn from '../../components/Teclado/index';
 
 export default function ConversorMoedas() {
 
@@ -64,14 +65,13 @@ export default function ConversorMoedas() {
 
   return (
     <View style={styles.container}>
-      
+    <View style={styles.containerCards}>
       {/* Card 1 */}
       <View style={styles.card}>
         <View style={styles.cardTop}>
           <View style={styles.imgView}>
             <Image src={currencyReq.flagImg} style={styles.flagImg} />
           </View>
-
           <View style={styles.textsView}>
             <TextRegular
               text={currencyReq.currency}
@@ -83,12 +83,10 @@ export default function ConversorMoedas() {
             />
           </View>
         </View>
-
         {/* Botao que abre a modal que muda a moeda de conversão */}
         <View style={styles.modalButton}>
           <AlterarValor data={DATA_CURRENCY} getValue={setCurrencyReq} resetField={setValueReq} />
         </View>
-
         {/* Input que mostra o valor selecionado para conversão */}
         <TextInput
           style={styles.inputCard}
@@ -100,7 +98,6 @@ export default function ConversorMoedas() {
           placeholderTextColor='#ACB1B2'
         />
       </View>
-
       {/* Botao de inverter os valores de conversão */}
       <TouchableOpacity
         style={styles.switchBtn}
@@ -112,14 +109,12 @@ export default function ConversorMoedas() {
       >
         <Octicons name="arrow-switch" size={24} color="#fff" />
       </TouchableOpacity>
-
       {/* Card 2 */}
       <View style={styles.card}>
         <View style={styles.cardTop}>
           <View style={styles.imgView}>
             <Image src={currencyRes.flagImg} style={styles.flagImg} />
           </View>
-
           <View style={styles.textsView}>
             <TextRegular
               text={currencyRes.currency}
@@ -131,12 +126,10 @@ export default function ConversorMoedas() {
             />
           </View>
         </View>
-
         {/* Botao que abre a modal que muda a moeda de conversão */}
         <View style={styles.modalButton}>
           <AlterarValor data={DATA_CURRENCY} getValue={setCurrencyRes} resetField={setValueRes} />
         </View>
-
         {/* Input que mostra o valor selecionado para conversão */}
         <TextInput
           style={styles.inputCard}
@@ -144,30 +137,85 @@ export default function ConversorMoedas() {
           value={valueRes}
         />
       </View>
-
+    </View>
+      
+    <View style = {styles.barra}/>
+      {/* Teclado */}
+      <View style = {styles.containerbutton}>
+          <View style = {styles.button}>
+            <Btn onPress={() => {}} value = {7}>
+              <TextBold text = "7" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}}value = {8}>
+              <TextBold text = "8" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}}value = {9}>
+              <TextBold text = "9" style = {styles.p}/>
+            </Btn>
+          </View>
+          <View style = {styles.button}>
+          <Btn onPress={() => {}}value = {4}>
+              <TextBold text = "4" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}}value = {5}>
+              <TextBold text = "5" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}}value = {6}>
+              <TextBold text = "6" style = {styles.p}/>
+            </Btn>
+          </View>
+          <View style = {styles.button}>
+          <Btn onPress={() => {}} value = {1}>
+              <TextBold text = "1" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}} value = {2}>
+              <TextBold text = "2" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}} value = {3}>
+              <TextBold text = "3" style = {styles.p}/>
+            </Btn>
+          </View>
+          <View style = {styles.button}>
+            <Btn onPress={() => {}} value = {0}>
+              <TextBold text = "0" style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}} value = ".">
+              <TextBold text = "." style = {styles.p}/>
+            </Btn>
+            <Btn onPress={() => {}} >
+              <Feather name="delete" size={24} color="#fff" />
+            </Btn>
+          </View>
+      </View>
       
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#0D0D0D',
     flexDirection: 'column',
-    gap: 25,
+    gap: 10,
+    flex: 1,
+    paddingTop: 35,
+  },
+  containerCards:{
+    backgroundColor: '#0D0D0D',
+    flexDirection: 'column',
+    gap: 10,
     flex: 1,
     padding: 15,
-    paddingTop: 45
   },
   p: {
-    color: '#fff'
+    color: '#fff',
+    fontSize: 20,
   },
   card: {
     backgroundColor: '#393939',
     flexDirection: 'column',
     borderRadius: 8,
     padding: 15,
-    gap: 15
+    gap: 15,
   },
   cardTop: {
     flexDirection: 'row',
@@ -203,5 +251,22 @@ const styles = StyleSheet.create({
     height: 45,
     width: 45,
     borderRadius: 8
+  },
+  button: {
+     flexDirection: 'row',
+     gap: 20,
+  },
+  containerbutton: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 6,
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    backgroundColor: '#05080A',
+    marginTop: 1,
+  },
+  barra:{
+    backgroundColor: '#2E9FB6',
+    height: 2,
   }
-});
+})
